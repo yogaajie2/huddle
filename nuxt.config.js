27fe75e957869016a1bb01411e0f6cf66ca3d4de
yogaajie2 <yogaajie2@gmail.com>
2021-06-18 13:52:09 +0700

    build: Remove router config

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,9 +1,9 @@
 export default {
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
-  router: {
-    base: '/huddle/'
-  },
+  // router: {
+  //   base: '/huddle/'
+  // },
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
